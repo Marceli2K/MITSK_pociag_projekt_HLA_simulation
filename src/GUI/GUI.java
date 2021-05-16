bec4c3d9cb06d2085c88ef9ec7554bfd7f9bfec1
Zaratrusztra <62715752+Zaratrusztra@users.noreply.github.com>
2021-05-16 14:09:35 +0200

    Initial commit

---
+++ src/GUI/GUI.java
@@ -0,0 +1,33 @@
+package GUI;
+
+import java.util.Random;
+
+/**
+ * Created by Stanislaw on 08.05.2018.
+ */
+public class GUI {
+    int timeToNext;
+    private Random random;
+
+    public GUI() {
+        random = new Random();
+        timeToNext = generateTimeToNext();
+    }
+
+    public int produce()
+    {
+        timeToNext=generateTimeToNext();
+        int count = random.nextInt(4)+1;
+        System.out.println("I produced " + count + ". Next I'll produce in " + timeToNext);
+        return count;
+    }
+
+    public int getTimeToNext() {
+        return timeToNext;
+    }
+
+    private int generateTimeToNext()
+    {
+        return random.nextInt(10)+1;
+    }
+}
